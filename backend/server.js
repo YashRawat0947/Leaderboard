b0fd07d9b253e80267f01171edf8da2c2870efd3
YashRawat0947 <147089324+YashRawat0947@users.noreply.github.com>
2024-10-15 17:37:56 +0530

    vs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('../config/db');
-const userRoutes = require('../routes/userRoutes');
-
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 connectDB();
@@ -14,4 +13,5 @@ app.use(cors());
 
 app.use('/api', userRoutes);
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
