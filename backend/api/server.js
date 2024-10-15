2eb00eeb657a5643fce664c39ae5776ac314e985
YashRawat0947 <147089324+YashRawat0947@users.noreply.github.com>
2024-10-15 17:56:11 +0530

    kevnj

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+const userRoutes = require('./routes/userRoutes'); // Adjust the path as needed
 
 dotenv.config();
-connectDB();
+connectDB(); // Connect to MongoDB
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Routes
 app.use('/api', userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+// Remove app.listen()
+module.exports = app;
